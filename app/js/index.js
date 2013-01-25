01c24e3817e2ca9f9abeffb00363910ba055533e
Cameron Bytheway <cameron@nujii.com>
2013-01-25 00:07:11 -0700

    Rename the angular app

--- app/js/index.js
+++ app/js/index.js
@@ -2,11 +2,14 @@
  * Module dependencies
  */
 var angular = require("angular")
+  , package = require("../../package.json");
+
+// We need angular-resource for our User service
 require("angular-resource");
 
 /*
  * Expose the app
  */
-module.exports = angular.module("app", [
+module.exports = angular.module(package.name, [
   "ngResource"
 ]);
