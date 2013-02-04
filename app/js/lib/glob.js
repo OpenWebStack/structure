c29378b6c95f6d6a8f26428305b8bf5414f7cfe9
Dave Geddes <davidcgeddes@gmail.com>
2013-02-03 20:52:26 -0700

    requirejs-glob node module

--- app/js/lib/glob.js
+++ app/js/lib/glob.js
@@ -1,32 +1,39 @@
 // var env = this['window'] ? 'browser' : 'node';
 /*TODO
-  -think of better name for service, and place to put it. only want in dev also
   -write() for the build
 */
 
-define(['text'], function(text){
+define(function(text){
   
   return {
     buildMap:{},
     load:function(name, req, load, config) {
-      //request matching files from Node service
       var files;
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'require-glob?glob=' + name);
-      xhr.send();
-      xhr.onreadystatechange = function(){
-        if (xhr.readyState === 4){
-          files = JSON.parse(xhr.responseText);
-          //load all the files with RequireJS
-          req(files, function(){
-            load(files);
-          });
-        }
-      };
-    },
-
-    write:function(pluginName, name, write) {
-      //TODO
+      if (config.isBuild){
+        //request matching files from Node lib
+        var glob = require.nodeRequire('requirejs-glob');
+        files = glob.match(name);
+        load(files);
+      }
+      else{
+        //request matching files from Node web service
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'requirejs-glob?glob=' + name);
+        xhr.send();
+        xhr.onreadystatechange = function(){
+          if (xhr.readyState === 4){
+            files = JSON.parse(xhr.responseText);
+            //load all the files with RequireJS
+            req(files, function(){
+              load(files);
+            });
+          }
+        };
+      }
     }
+
+    // write:function(pluginName, name, write) {
+    //   console.log('writing');
+    // }
   };
 });
--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ var app = express();
 //here's how to set things that routes may need access to (drivers, loggers, etc)
 app.set('info', {name: "Open Web App"});
 
-//all environments
-app.configure(function(){
+//development environment
+app.configure('development', function(){
   //serve the static assets
   app.use(express.static('app'));
+  //use requirejs-glob middleware
+  app.use(require('requirejs-glob').middleware('app/js/'));
 });
 
 //production environment
@@ -24,29 +26,6 @@ app.use(require('./middleware/logging')());
 //routes
 fs.readdirSync(__dirname + '/routes').forEach(function(file) {
   require('./routes/' + file)(app);
-});
-
-//require-on-steroids
-/*TODO
-  -make globFrom configurable
-  -move into own package & repo
-  -only load in development
-  -consider standalone for non-node backends
-*/
-var globsync = require('glob-whatev');
-var path = require('path');
-app.get('/require-glob', function(req, res){
-  var files = [];
-  var glob = req.query.glob;
-  var globFrom = 'app/js/';
-  globsync.glob(globFrom + glob).forEach(function(filepath) {
-    //strip off the globFrom
-    filepath = filepath.split(globFrom)[1];
-    //strip of the extension
-    filepath = filepath.split(path.extname(filepath))[0];
-    files.push(filepath);
-  });
-  res.send(files);
 });
 
 app.listen(3000);
