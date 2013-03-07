8a52d7e4d0905b18bba160f5fcb469b158309f6b
aaronfrost@ldschurch.org <nrmacsnrmnrmtm>
2013-03-07 16:13:22 -0700

    Added the live reload stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,35 @@
+//Global Dependencies for LiveReload
+// var path = require('path');
+// var lrSnippet = ;
+
+// var folderMount = function folderMount(connect, point) {
+//   return connect.static(path.resolve(point));
+// };
+
 module.exports = function(grunt){
   //grunt plugins
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-clear');
-  grunt.loadNpmTasks('gruntacular');
-  grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-angular-templates');
+  grunt.loadNpmTasks('grunt-clear');
   grunt.loadNpmTasks('grunt-htmlrefs');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('gruntacular');
+  //Live Reload Plugins
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
 
   //config
   grunt.initConfig({
     watch: {
       //run unit tests with testacular (server needs to be already running)
-      testacular: {
-        files: ['app/js/**/*.js', 'test/browser/**/*.js'],
-        tasks: ['testacular:unit:run']
-      },
-      node: {
-        files: ['server/**/*.js', 'test/node/**/*.js'],
-        tasks: ['simplemocha']
-      },
-      stylus: {
-        files: ['app/styles/**/*.styl'],
-        tasks: ['stylus']
-      }
+      
     },
 
     //for tests that run in browsers
@@ -140,10 +141,41 @@ module.exports = function(grunt){
         src: ['app/app.build.js'],
         dest: 'build/app.min.js'
       }
+    },
+
+    regarde: {
+      dev: {
+        files: [
+            'app/js/**/*.js',
+            'app/styles/**/*.css',
+            'app/**/*.html'
+        ],
+        tasks: ['livereload']
+      },
+      testacular: {
+        files: ['app/js/**/*.js', 'test/browser/**/*.js'],
+        tasks: ['testacular:unit:run']
+      },
+      stylus: {
+        files: ['app/styles/**/*.styl'],
+        tasks: ['stylus']
+      }
+    },
+    connect: {
+      livereload: {
+        options: {
+          port: 9001,
+          middleware: function(connect, options) {
+            return [require('grunt-contrib-livereload/lib/utils').livereloadSnippet, (function(c,p) {
+              return c.static(require('path').resolve(p));})(connect, '.')]
+          }
+        }
+      }
     }
 
   });
 
   grunt.registerTask('test', ['testacular:continuous', 'simplemocha']);
   grunt.registerTask('build', ['clean', 'stylus', 'copy', 'ngtemplates', 'htmlrefs', 'htmlmin', 'concat', 'uglify']);
+  grunt.registerTask('livetasks', ['livereload-start', 'connect:livereload', 'regarde']);
 };
